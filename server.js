fa3791b738bab27902a50de66b1518aa3846edfd
mkbowen78 <mkbowen78@gmail.com>
2017-11-08 14:21:59 -0600

    various fixes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require("path");
 
 // Sets up the Express App
 // =============================================================
-var port = process.env.PORT || 3000;
+var port = process.env.port || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
