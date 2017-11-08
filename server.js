bb2d378ba0bb6c960eafd68de0e1240351d87c07
M. Kevin Bowen <kevin@iambowen.com>
2017-11-08 14:23:29 -0600

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ var path = require("path");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = 3000;
+var port = process.env.port || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -89,6 +88,6 @@ app.post("/api/new", function(req, res) {
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+app.listen(port, function() {
+  console.log("App listening on port " + port);
 });
